38539c7952856dcc5b7748924de701779e131371
Collin Driscoll <cdriscoll@akitabox.com>
2018-08-31 13:53:30 -0500

    feat(app): add proof-of-concept json loading
    update deps, add vscode to gitignore

--- src/app.tsx
+++ src/app.tsx
@@ -1,2 +1,28 @@
+// Bring in polyfills required by babel and React
 import "@babel/polyfill";
 import "raf/polyfill";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as _env from "./settings.env.json";
+import { EnvConfig } from "./types/EnvConfig";
+
+const env : EnvConfig = _env as EnvConfig;
+
+export default class HalloweenApp extends React.Component<
+  {
+    env: EnvConfig
+  },
+  {}
+> {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <span className="appName">{this.props.env.version}</span>;
+  }
+}
+
+ReactDOM.render(
+  <HalloweenApp env={env} />,
+  document.getElementById("app")
+);
---
+++ src/types/EnvConfig.ts
@@ -0,0 +1,4 @@
+export interface EnvConfig {
+  env: string,
+  version: string
+}
