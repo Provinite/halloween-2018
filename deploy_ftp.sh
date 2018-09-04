ec0d7ef91edaa1496425abfb704d86d2c84f1b3c
prov <provinite@gmail.com>
2018-09-03 19:57:01 -0500

    attempted ftp deploy

---
+++ deploy_ftp.sh
@@ -0,0 +1,10 @@
+shopt -s globstar
+cd packages/web-client/dist
+for f in ./**;
+do
+  if [[ -d $f ]]; then
+    continue;
+  fi
+  sftp_add_path=`dirname $f`/;
+  curl --ftp-create-dirs -T $f --user ${sftp_user}:${sftp_password} ftp://${sftp_host}${sftp_path}${sftp_add_path};
+done;
