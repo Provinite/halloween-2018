9fcae8a4db816ac17a616d49775088461e222b7a
prov <provinite@gmail.com>
2018-10-09 20:04:00 -0500

    feat(api/AwilixHelpers): add asStaticMethod resolver
    for making DI-proxied calls to static class methods (such as configureContainer). abstract some
    implementation logic in AwilixHelpers
    
    prep work for #34 adding env service

--- packages/api/src/AwilixHelpers.ts
+++ packages/api/src/AwilixHelpers.ts
@@ -8,16 +8,48 @@
  * @param method - The method to wrap.
  * @return A modified version of method.bind(instance) that is friendly towards
  *    Awilix's notion of a "function".
+ * @example container.build(asClassMethod(someService, someService.someMethod))
  */
 export function asClassMethod(
   instance: any,
   method: (...args: any[]) => any
 ): (...args: any[]) => any {
+  // Bind the function to the instance, so that `this` is correctly set.
   const injectable = method.bind(instance);
-  // Awilix looks for something like "function ${name}" to begin parsing
-  // so we just mock it up like that.
   // Function.name is read-only, so modify its property descriptor directly.
+  // otherwise it gets left with something like "bound ${name}"
   Object.getOwnPropertyDescriptor(injectable, "name").value = method.name;
-  injectable.toString = () => "function " + method.toString();
+  makeProxyAwilixFriendly(injectable, method);
   return injectable;
+}
+
+/**
+ * Create a resolver for a static class method.
+ * @param method The static method to register
+ * @example container.build(asStaticMethod(SomeClass.someStaticMethod))
+ */
+export function asStaticMethod(method: (...args: any[]) => any) {
+  // Create a proxy function so we don't pollute the real function's
+  // toString() method.
+  const injectable = (...args: any[]) => method(...args);
+  makeProxyAwilixFriendly(injectable, method);
+  return injectable;
+}
+
+/* Private Functions */
+/**
+ * When Awilix is parsing functions for classic mode DI injection, it analyzes
+ * the result of `fn.toString()`. This does not play well with typescript class
+ * methods since their toString method looks like `functionName(args)` instead of
+ * `function functionName(args)` or `(args) =>`. This function attaches a
+ * toString() method to the given injectable proxy, which provides a result
+ * that can be properly parsed for classic DI injection.
+ * @param injectable - The proxy to modify.
+ * @param realFunction - The underlying function to proxy for injection.
+ */
+function makeProxyAwilixFriendly(
+  injectable: (...args: any[]) => any,
+  realFunction: (...args: any[]) => any
+) {
+  injectable.toString = () => "function " + realFunction.toString();
 }
