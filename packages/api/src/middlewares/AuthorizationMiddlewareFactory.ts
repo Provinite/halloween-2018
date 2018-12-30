c4379a970c7d7e098f0aba0d5162772bc10b49dc
prov <provinite@gmail.com>
2018-12-30 17:21:11 -0600

    refactor(api/AuthorizationMiddlewareFactory): refactor usage of lookupRoute

--- packages/api/src/middlewares/AuthorizationMiddlewareFactory.ts
+++ packages/api/src/middlewares/AuthorizationMiddlewareFactory.ts
@@ -4,6 +4,7 @@ import { Repository } from "typeorm";
 import { AuthenticationService } from "../auth/AuthenticationService";
 import { hasRole } from "../auth/AuthHelpers";
 import { PermissionDeniedError } from "../auth/PermissionDeniedError";
+import { RoleLiteral } from "../auth/RoleLiteral";
 import { getMethod } from "../HttpMethod";
 import { User } from "../models";
 import { RouteRegistry } from "../web/RouteRegistry";
@@ -23,18 +24,24 @@ export class AuthorizationMiddlewareFactory implements IMiddlewareFactory {
     return async (ctx: Context, next: () => Promise<any>) => {
       const { path, method } = ctx;
       const requestContainer: AwilixContainer = ctx.state.requestContainer;
-      const { allowedRoles, error } = this.routeRegistry.lookupRoute(
-        path,
-        getMethod(method)
-      );
-      if (error) {
-        // allow processing to continue
+      let allowedRoles: RoleLiteral[];
+      try {
+        allowedRoles = this.routeRegistry.lookupRoute(path, getMethod(method))
+          .allowedRoles;
+      } catch (e) {
+        // TODO: Relying on a downstream middleware to handle this isn't the best
+        // maybe we should have a middleware that handles 404s and such up-front.
+
+        // unable to get permission information for the request, so allow
+        // it to continue on.
         return await next();
       }
       // parse the token from the authorization header
       const token = ctx.get("Authorization").replace("Bearer ", "");
-      let allow = false;
-      let user;
+      /** if true, allow the request through */
+      let allow: boolean = false;
+      /** the current user */
+      let user: User;
       if (token) {
         // try to authenticate
         // let any auth errors bubble up
