1a31c19fd234abf76f5a45bb425620d860a028ce
prov <provinite@gmail.com>
2018-12-30 16:59:07 -0600

    feat(api/MethodNotSupportedError): add allow property

--- packages/api/src/web/MethodNotSupportedError.ts
+++ packages/api/src/web/MethodNotSupportedError.ts
@@ -1,4 +1,15 @@
+import { HttpMethod } from "../HttpMethod";
+
 /**
  * Error indicating that the requested method is not supported for this route.
  */
-export class MethodNotSupportedError extends Error {}
+export class MethodNotSupportedError extends Error {
+  /**
+   * Array of allowed http methods for the route.
+   */
+  readonly allow: HttpMethod[];
+  constructor(allow: HttpMethod[]) {
+    super();
+    this.allow = allow;
+  }
+}
