7cf85195459f137e455ba7e76b9ceeb220105b4a
Collin Driscoll <provinite@gmail.com>
2019-02-09 17:07:12 -0600

    docs(api/AuthenticationError): add better docs

--- packages/web-client/src/services/auth/AuthenticationError.ts
+++ packages/web-client/src/services/auth/AuthenticationError.ts
@@ -1,4 +1,6 @@
 /**
- * Error thrown indicating that authentication failed.
+ * Error thrown indicating that authentication of
+ * a request failed. For example, due to an expired or
+ * missing token.
  */
 export class AuthenticationError extends Error {}
