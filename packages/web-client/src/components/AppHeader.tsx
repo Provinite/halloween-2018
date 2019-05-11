1f1bfbd7aaed78abf5897335a97aadd61c428f74
Collin Driscoll <provinite@gmail.com>
2019-05-11 11:33:48 -0500

    feat(web-client/AppHeader): add children slot

--- packages/web-client/src/components/AppHeader.tsx
+++ packages/web-client/src/components/AppHeader.tsx
@@ -4,12 +4,13 @@ import * as React from "react";
 /**
  * Functional component presenting the shared top bar for the app.
  */
-export const AppHeader = () => (
+export const AppHeader: React.SFC = ({ children }) => (
   <AppBar position="relative" color="primary">
     <Toolbar>
       <Typography variant="h6" color="inherit">
         Pillowing Pile
       </Typography>
+      {children}
     </Toolbar>
   </AppBar>
 );
