50fdfd242e89b50146e41c1a223f9e7535677ce1
prov <provinite@gmail.com>
2019-05-12 17:46:52 -0500

    refactor(api/transactionService): refactor for proxy-based injection

--- packages/api/src/db/TransactionService.ts
+++ packages/api/src/db/TransactionService.ts
@@ -1,10 +1,18 @@
-import { asValue, AwilixContainer } from "awilix";
+import { asValue } from "awilix";
 import { Connection } from "typeorm";
+import {
+  AnyContext,
+  ApplicationContainer
+} from "../config/context/ApplicationContext";
+import { RequestContainer } from "../config/context/RequestContext";
 import { Component } from "../reflection/Component";
 
 @Component("SCOPED")
 export class TransactionService {
-  constructor(private container: AwilixContainer) {}
+  private container: ApplicationContainer | RequestContainer;
+  constructor({ container }: AnyContext) {
+    this.container = container;
+  }
   /**
    * Run a function in a transaction.
    * @param fn - The function to execute, will be built with a DI container.
