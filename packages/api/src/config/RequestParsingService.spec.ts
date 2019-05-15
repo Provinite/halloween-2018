1eee28fe84eb644d41bc7baf3bb9c6e7b531caa3
prov <provinite@gmail.com>
2019-05-14 20:55:02 -0500

    test(api/RequestParsingService): fix up tests

--- packages/api/src/config/RequestParsingService.spec.ts
+++ packages/api/src/config/RequestParsingService.spec.ts
@@ -7,7 +7,7 @@ import { RequestParsingService } from "./RequestParsingService";
 describe("service:RequestParsingService", () => {
   describe("method:parse", () => {
     it("registers the provided body as requestBody", () => {
-      jest.spyOn(Awilix, "asValue").mockImplementation(mockAsValue);
+      jest.spyOn(Awilix, "asValue").mockImplementation(mockAsValue as any);
       const container = ({
         register: jest.fn()
       } as unknown) as jest.Mocked<AwilixContainer>;
@@ -23,7 +23,7 @@ describe("service:RequestParsingService", () => {
         expect.anything()
       );
       const args = container.register.mock.calls[0];
-      const registeredValue = args[1];
+      const registeredValue = (args as any)[1];
       expect(registeredValue).toBeMockValueResolverFor(ctx.request.body);
     });
   });
