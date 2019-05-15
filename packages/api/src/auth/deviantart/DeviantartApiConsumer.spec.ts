14560568679e5c5a81624b14c21f7457b6e69cc5
prov <provinite@gmail.com>
2019-05-14 19:52:53 -0500

    test(api/deviantartApiConsumer): fix up tests

--- packages/api/src/auth/deviantart/DeviantartApiConsumer.spec.ts
+++ packages/api/src/auth/deviantart/DeviantartApiConsumer.spec.ts
@@ -1,6 +1,8 @@
 import Axios from "axios";
+import { ApplicationContext } from "../../config/context/ApplicationContext";
 import { EnvService } from "../../config/env/EnvService";
 import { IDeviantartApiConsumerConfiguration } from "../../config/env/IDeviantartApiConsumerConfig";
+import { createSafeContext } from "../../test/testUtils";
 import { DeviantartApiConsumer } from "./DeviantartApiConsumer";
 describe("service:DeviantartApiConsumer", () => {
   afterEach(() => {
@@ -30,13 +32,18 @@ describe("service:DeviantartApiConsumer", () => {
         getDeviantartApiConsumerConfig: () => mocks.config
       } as any;
 
+      mocks = createSafeContext(mocks);
+
       /* Stubs */
-      jest.spyOn(Axios, "post").mockImplementation(() => ({
-        data: "result"
-      }));
+      jest.spyOn(Axios, "post").mockImplementation(
+        () =>
+          ({
+            data: "result"
+          } as any)
+      );
 
       /* Default Service */
-      svc = new DeviantartApiConsumer(mocks.envService);
+      svc = new DeviantartApiConsumer((mocks as unknown) as ApplicationContext);
     });
     it("makes a POST request to the oauth endpoint", async () => {
       await svc.authenticate("boop");
