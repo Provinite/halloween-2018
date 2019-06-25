427cc392dd955bb2948895c29ce8b66de8ed1dab
prov <provinite@gmail.com>
2019-06-24 22:57:02 -0500

    feat(api/models): update models barrel

--- packages/api/src/models/index.ts
+++ packages/api/src/models/index.ts
@@ -9,6 +9,9 @@ export { User } from "./User";
 import { Repository } from "typeorm";
 import { Game } from "./Game";
 export { Game } from "./Game";
+import { LocalCredentials } from "./LocalCredentials";
+import { DeviantartAccount } from "./DeviantartAccount";
+export { LocalCredentials } from "./LocalCredentials";
 /**
  * Interface representing a model constructor function.
  */
@@ -16,7 +19,15 @@ export interface IModelClass<T = any> {
   createInitialEntities?: (...args: any[]) => Promise<any>;
   new (...args: any[]): T;
 }
-export const MODELS: IModelClass[] = [User, Prize, Role, DrawEvent, Game];
+export const MODELS: IModelClass[] = [
+  User,
+  Prize,
+  Role,
+  DrawEvent,
+  Game,
+  LocalCredentials,
+  DeviantartAccount
+];
 
 declare global {
   interface ApplicationContextMembers {
@@ -24,7 +35,6 @@ declare global {
     // if a custom repository is created for any model in this list,
     // remove it from here and declare its more specific type in the
     // implementation file
-    userRepository: Repository<User>;
     roleRepository: Repository<Role>;
     gameRepository: Repository<Game>;
   }
